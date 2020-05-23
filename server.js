aba1872f017d00559ec38712daaea62b1e95b25f
william <williamrochajr@gmail.com>
2020-05-22 23:47:13 -0300

    criando o model de produto

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
@@ -7,8 +8,17 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useUnifiedTopology: true
 });
 
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
 app.get("/", (req, res) => {
-  res.send("hello ");
+  Product.create({
+    title: "react native",
+    description: "build native",
+    url: "http://github.com/facebook/react-native"
+  });
+  return res.send("hello ");
 });
 
 app.listen(3001);
---
+++ src/models/Product.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  description: {
+    type: String,
+    require: true
+  },
+  url: {
+    type: String,
+    require: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model("Product", ProductSchema);
+
