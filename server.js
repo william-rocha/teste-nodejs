aea2322e4ba484ac5a4b4acb2701692bd2d66219
william <williamrochajr@gmail.com>
2020-05-22 22:59:04 -0300

    conectando ao database com mongoose

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello ");
 });
 
 app.listen(3001);
